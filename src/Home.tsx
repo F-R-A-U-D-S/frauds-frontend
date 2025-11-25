8e4299b33292d23ab154edf1ca5759fc2aecb374
tanasije286 <tanasije286@gmail.com>
2025-11-24 21:55:25 -0500

    Added Route to download page in Home.tsx

--- src/Home.tsx
+++ src/Home.tsx
@@ -4,10 +4,19 @@ import './pages/Login.css'
 import Dashboard from "./pages/Dashboard";
 import PrivateRoute from "./auth/PrivateRoute";
 import PublicRoute from "./auth/PublicRoute";
+import DownloadPage from "./pages/DownloadPage";
 
 export default function Home() {
   return (
     <Routes>
+      <Route
+        path="/download"
+        element={
+          <PublicRoute>
+            <DownloadPage />
+          </PublicRoute>
+        }
+      />
       <Route
         path="/login"
         element={
