da3f1ae5c8cf6a2cb5e42eb807ba64f0f9b86924
Matt N <97564308+afk-m@users.noreply.github.com>
2026-01-16 16:58:29 -0500

    Updated local and ec2 urls

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,3 @@
-//export const API_BASE_URL = "http://44.197.183.233:8000";
-export const API_BASE_URL = "http://127.0.0.1:8000";
+// Default to local URL
+export const API_BASE_URL =
+  import.meta.env.VITE_API_BASE_URL ?? "http://127.0.0.1:8000";
