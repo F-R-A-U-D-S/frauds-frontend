d6f32dea4ab7c3621fcd44d7c23ef497ee4b9283
tanasije286 <tanasije286@gmail.com>
2026-01-29 19:30:33 -0500

    changed calling for downloading csv

--- src/api/downloadWrappers.tsx
+++ src/api/downloadWrappers.tsx
@@ -2,7 +2,7 @@ import { handleFileDownload } from "./downloadResult";
 
 export function handleDownloadCsv(key: string) {
   return handleFileDownload(key, {
-    url: "/predict/download",
+    url: "/predict/download/csv",
     accept: "text/csv",
     mimeType: "text/csv",
     fallbackFilename: "flagged_results.csv",
